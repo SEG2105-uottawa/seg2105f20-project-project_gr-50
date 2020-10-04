f58a70a790026902a8cae760722b14103ed8f0fb
Jiadong Yu <jyu150@uottawa.ca>
2020-10-04 00:48:35 -0400

    adding new class Person

---
+++ app/src/main/java/seg2105/project50/novigrad/Person.java
@@ -0,0 +1,27 @@
+package seg2105.project50.novigrad;
+
+public class Person {
+    private String name;
+    private String role;
+    private String email;
+    private String password;
+
+    Person(String name, String role, String email, String password){
+        this.name = name;
+        this.role = role;
+        this.email = email;
+        this.password = password;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getRole(){
+        return role;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getPassword(){
+        return password;
+    }
+}
