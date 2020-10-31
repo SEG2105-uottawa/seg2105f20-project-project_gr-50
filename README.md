3a9ac4cce7850d75e2660d9741c051eea54e1cb7
Hamidou <hciss015@gmail.com>
2020-10-31 02:12:30 -0400

    Feature: Add more services, up to 8

--- README.md
+++ README.md
@@ -3,8 +3,10 @@ Deliverable 2
 Student Names
 1) Hamidou Nouhoum Cisse
 2) Jiadong Yu
-3) Gerald Azichoba 
+3) Gerald Azichoba
+
+The credentials for the admin account are:
+ email/pseudo : admin
+ password : admin
 
 We have incorporated the circlCi building badge for bonus.
-
-
--- app/src/main/java/seg2105/project50/novigrad/ServicesAvailable.java
+++ app/src/main/java/seg2105/project50/novigrad/ServicesAvailable.java
@@ -22,13 +22,25 @@ public class ServicesAvailable extends AppCompatActivity {
     private FirebaseAuth auth;
     private FirebaseUser user;
     private DatabaseReference database;
+
     private ServicesSettings service;
     private ServicesSettings service2;
     private ServicesSettings service3;
+    private ServicesSettings service4;
+    private ServicesSettings service5;
+    private ServicesSettings service6;
+    private ServicesSettings service7;
+    private ServicesSettings service8;
+
 
     Button btn_serv1;
     Button btn_serv2;
     Button btn_serv3;
+    Button btn_serv4;
+    Button btn_serv5;
+    Button btn_serv6;
+    Button btn_serv7;
+    Button btn_serv8;
 
 
     @Override
@@ -40,6 +52,11 @@ public class ServicesAvailable extends AppCompatActivity {
         btn_serv1 = (Button)findViewById(R.id.btn_service1);
         btn_serv2 = (Button)findViewById(R.id.btn_service2);
         btn_serv3 = (Button)findViewById(R.id.btn_service3);
+        btn_serv4 = (Button)findViewById(R.id.btn_service4);
+        btn_serv5 = (Button)findViewById(R.id.btn_service5);
+        btn_serv6 = (Button)findViewById(R.id.btn_service6);
+        btn_serv7 = (Button)findViewById(R.id.btn_service7);
+        btn_serv8 = (Button)findViewById(R.id.btn_service8);
 
 
 
@@ -117,6 +134,127 @@ public class ServicesAvailable extends AppCompatActivity {
                     }
                 });
 
+        database.child("Services")
+                .child("Service 4")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange( DataSnapshot dataSnapshot) {
+                        service4 = dataSnapshot.getValue(ServicesSettings.class);
+                        if(service4!= null) {
+                            if (!service4.isActive())
+                                btn_serv4.setVisibility(View.GONE);
+                            String name = service4.getName();
+                            btn_serv4.setText(name);
+                        }
+                        else{
+                            startActivity(new Intent(getApplicationContext(), HomePage.class)); //added
+                            finish();
+                            Toast.makeText(getApplicationContext(), "Something went wrong...",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+
+        database.child("Services")
+                .child("Service 5")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange( DataSnapshot dataSnapshot) {
+                        service5 = dataSnapshot.getValue(ServicesSettings.class);
+                        if(service5!= null) {
+                            if (!service5.isActive())
+                                btn_serv5.setVisibility(View.GONE);
+                            String name = service5.getName();
+                            btn_serv5.setText(name);
+                        }
+                        else{
+                            startActivity(new Intent(getApplicationContext(), HomePage.class)); //added
+                            finish();
+                            Toast.makeText(getApplicationContext(), "Something went wrong...",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+
+        database.child("Services")
+                .child("Service 6")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange( DataSnapshot dataSnapshot) {
+                        service6 = dataSnapshot.getValue(ServicesSettings.class);
+                        if(service6!= null) {
+                            if (!service6.isActive())
+                                btn_serv6.setVisibility(View.GONE);
+                            String name = service6.getName();
+                            btn_serv6.setText(name);
+                        }
+                        else{
+                            startActivity(new Intent(getApplicationContext(), HomePage.class)); //added
+                            finish();
+                            Toast.makeText(getApplicationContext(), "Something went wrong...",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+
+        database.child("Services")
+                .child("Service 7")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange( DataSnapshot dataSnapshot) {
+                        service7 = dataSnapshot.getValue(ServicesSettings.class);
+                        if(service7!= null) {
+                            if (!service7.isActive())
+                                btn_serv7.setVisibility(View.GONE);
+                            String name = service7.getName();
+                            btn_serv7.setText(name);
+                        }
+                        else{
+                            startActivity(new Intent(getApplicationContext(), HomePage.class)); //added
+                            finish();
+                            Toast.makeText(getApplicationContext(), "Something went wrong...",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+
+        database.child("Services")
+                .child("Service 8")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange( DataSnapshot dataSnapshot) {
+                        service8 = dataSnapshot.getValue(ServicesSettings.class);
+                        if(service8!= null) {
+                            if (!service8.isActive())
+                                btn_serv8.setVisibility(View.GONE);
+                            String name = service8.getName();
+                            btn_serv8.setText(name);
+                        }
+                        else{
+                            startActivity(new Intent(getApplicationContext(), HomePage.class)); //added
+                            finish();
+                            Toast.makeText(getApplicationContext(), "Something went wrong...",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+
+
 
     }
 
@@ -138,6 +276,41 @@ public class ServicesAvailable extends AppCompatActivity {
         finish();
         Intent intent = new Intent(getApplicationContext(), ServiceInfoGather.class);
         intent.putExtra("ser_num", "Service 3");
+        startActivity(intent);
+    }
+
+    public void setService4(View view){
+        finish();
+        Intent intent = new Intent(getApplicationContext(), ServiceInfoGather.class);
+        intent.putExtra("ser_num", "Service 4");
+        startActivity(intent);
+    }
+
+    public void setService5(View view){
+        finish();
+        Intent intent = new Intent(getApplicationContext(), ServiceInfoGather.class);
+        intent.putExtra("ser_num", "Service 5");
+        startActivity(intent);
+    }
+
+    public void setService6(View view){
+        finish();
+        Intent intent = new Intent(getApplicationContext(), ServiceInfoGather.class);
+        intent.putExtra("ser_num", "Service 6");
+        startActivity(intent);
+    }
+
+    public void setService7(View view){
+        finish();
+        Intent intent = new Intent(getApplicationContext(), ServiceInfoGather.class);
+        intent.putExtra("ser_num", "Service 7");
+        startActivity(intent);
+    }
+
+    public void setService8(View view){
+        finish();
+        Intent intent = new Intent(getApplicationContext(), ServiceInfoGather.class);
+        intent.putExtra("ser_num", "Service 8");
         startActivity(intent);
     }
     public void takeHome(View view){
