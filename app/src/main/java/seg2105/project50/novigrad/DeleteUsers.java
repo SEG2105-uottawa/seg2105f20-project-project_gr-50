eda5e6d8c8346fd8fd7a6092688690c2d9fde374
Gerald Azichoba <gazic055@uottawa.ca>
2020-10-31 20:53:27 -0700

    Admin can see all users. (most pages can scroll)

--- app/src/main/java/seg2105/project50/novigrad/DeleteUsers.java
+++ app/src/main/java/seg2105/project50/novigrad/DeleteUsers.java
@@ -2,20 +2,29 @@ package seg2105.project50.novigrad;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DeleteUsers extends AppCompatActivity {
 
@@ -23,6 +32,10 @@ public class DeleteUsers extends AppCompatActivity {
     private EditText emailAddy;
     FirebaseAuth fb;
     FirebaseDatabase mDatabase;
+    ListView listviewUsers; //added
+    DatabaseReference databaseUsers;
+    List<String> users;
+    //StorageReference listRef; //added
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +44,13 @@ public class DeleteUsers extends AppCompatActivity {
 
         fb = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance();
-
+        databaseUsers = FirebaseDatabase.getInstance().getReference("Citizens");
         emailAddy = findViewById(R.id.UserToDelete);
+       listviewUsers = findViewById(R.id.allUsers); //added
+
+        users = new ArrayList<>(); //added
+
+
 
        // DatabaseReference dR ;
 
@@ -62,16 +80,6 @@ public class DeleteUsers extends AppCompatActivity {
 
 
 
-                        //if(dR.removeValue().isSuccessful()){
-                          //  Toast.makeText(DeleteUsers.this, string_emailAddy + " has been deleted.", Toast.LENGTH_SHORT).show();
-                       // }
-                         //else{
-                         //   Toast.makeText(DeleteUsers.this, "Alert "+string_emailAddy + " has not been deleted !!.", Toast.LENGTH_SHORT).show();
-                       // }
-
-                                    //mDatabase.getReference("Citizens").child(fb.getCurrentUser(string_emailAddy).getUid()).removeValue();
-                                    // mDatabase.getReference("Citizens").child().equalTo(string_emailAddy).removeValue();
-                                   // Toast.makeText(DeleteUsers.this, string_emailAddy + " has been deleted.", Toast.LENGTH_SHORT).show();
                     }
 
 
@@ -85,6 +93,30 @@ public class DeleteUsers extends AppCompatActivity {
                     Toast.makeText(DeleteUsers.this,  "No account has been deleted.", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(getApplicationContext(),DeleteUsers.class));
                 }
+
+            }
+        });
+
+    }
+
+    protected void onStart(){
+        super.onStart();;
+
+        databaseUsers.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                users.clear();
+
+                for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                    Person userID = postSnapshot.getValue(Person.class);
+                    users.add(userID.getEmail());
+                }
+                ArrayAdapter<String> namesOfUsers = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1 ,users);
+                listviewUsers.setAdapter(namesOfUsers);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
